4811aa806c89f9a7982b514f035382055f2a8d19
Vishnu4226 <52781872+Vishnu4226@users.noreply.github.com>
2023-05-17 09:47:42 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,4 +3,7 @@ resource "aws_instance" "ec2_instance" {
     subnet_id = "subnet-09fc1627de77451af"
     instance_type = "t2.large"
     key_name = "jenkins-terraform-test"
+    metadata_options {
+    http_tokens = "required"
+  } 
 }

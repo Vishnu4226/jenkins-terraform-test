fd31ad0cd9c53c30738f95d5ebdf1362e0363713
vishnu4772 <gonuguntavishnu@gmail.com>
2023-12-28 21:44:23 +0530

    deleting the unwanted file

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2_instance" {
+#resource "aws_instance" "ec2_instance" {
     ami = "ami-0bc72ee28ad771c22"
     subnet_id = "subnet-09fc1627de77451af"
     instance_type = "t2.large"
